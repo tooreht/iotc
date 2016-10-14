1a80627d870502074a9eb7e3385163f19fbf48fc
tooreht <tooreht@gmail.com>
2016-10-14 15:29:01 +0200

    Pattern match the UDP payload per fn param

--- apps/semtech/lib/semtech/decoder.ex
+++ apps/semtech/lib/semtech/decoder.ex
@@ -1,21 +1,17 @@
 defmodule Semtech.Decoder do
 
-  def parse_pkt_fwd_packet(data) do
-    packet_len = length(data)
-    IO.puts packet_len
-    reminder_size = packet_len * 8 - 32
-    IO.puts reminder_size
-
-    # <<
-    #   version       :: size(8),
-    #   random_token  :: size(16),
-    #   type          :: size(8),
-    #   reminder      :: size(reminder_size)
-    # >> = data
-
-    # case 0x00 do
-    #   ^type -> parse_push_data(version, random_token, type, reminder)
-    # end
+  def parse_pkt_fwd_packet(<<
+      version       :: size(8),
+      random_token  :: size(16),
+      type          :: size(8),
+      mac           :: size(64),
+      json          :: binary
+    >>) do
+    IO.puts version
+    IO.puts random_token
+    IO.puts type
+    IO.puts mac
+    IO.puts json
   end
 
   def parse_push_data(version, random_token, type, reminder) do
