8c50f27ce5ccba8c99e117e4a652fab59582a1e4
Wallcreeper <simon.wittwer@outlook.com>
2016-10-22 18:19:26 +0200

    Implement Application behaviour and Supervisor
    Add LoRaWAN.Handler module to supervision tree.

--- apps/lorawan/lib/lorawan.ex
+++ apps/lorawan/lib/lorawan.ex
@@ -1,2 +1,15 @@
 defmodule LoRaWAN do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(Handler, [LoRaWAN.Handler])
+    ]
+
+    # Start the main supervisor, and restart failed children individually
+    opts = [strategy: :one_for_one, name: LoRaWAN.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 end
