b5fc84c53a34b7e5f4227e2cb69c6545dae87c77
tooreht <tooreht@gmail.com>
2016-10-24 00:06:38 +0200

    Implement Semtech.Encoder

--- apps/semtech/lib/semtech/encoder.ex
+++ apps/semtech/lib/semtech/encoder.ex
@@ -1,3 +1,49 @@
 defmodule Semtech.Encoder do
-  # TODO: Implement!
+  @moduledoc """
+  This module is responsible for the encoding of the different packet types.
+  """
+
+  @doc """
+  Encodes a packet to it's binary format based on the identifier.
+  """
+  def encode(packet) do
+    case packet.identifier do
+      0x00 -> <<
+          packet.version    :: size(8),
+          packet.token      :: size(16),
+          0x00              :: size(8),
+          packet.gateway_id :: size(64),
+          packet.payload    :: binary
+        >>
+      0x01 -> <<
+          packet.version    :: size(8),
+          packet.token      :: size(16),
+          0x01              :: size(8)
+        >>
+      0x02 -> <<
+          packet.version    :: size(8),
+          packet.token      :: size(16),
+          0x02              :: size(8),
+          packet.gateway_id :: size(64)
+        >>
+      0x03 -> <<
+          packet.version    :: size(8),
+          packet.token      :: size(16),
+          0x03              :: size(8),
+          packet.payload    :: binary
+        >>
+      0x04 -> <<
+          packet.version    :: size(8),
+          packet.token      :: size(16),
+          0x04              :: size(8)
+        >>
+      0x05 -> <<
+          packet.version    :: size(8),
+          packet.token      :: size(16),
+          0x05              :: size(8),
+          packet.payload    :: binary
+        >>
+      _ -> nil
+    end
+  end
 end
