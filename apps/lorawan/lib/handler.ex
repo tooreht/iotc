751ce4f229c89cf6219e909b1f56699e19f67845
Wallcreeper <simon.wittwer@outlook.com>
2016-10-24 20:38:22 +0200

    Implement PHY layer parsing
    - Implement PHY layer packet structure
    - Implement parsing in handler

--- apps/lorawan/lib/handler.ex
+++ apps/lorawan/lib/handler.ex
@@ -1,5 +1,9 @@
-defmodule Handler do
+ defmodule Handler do
+  @moduledoc """
+  This module implements the LoRaWAN communication protocol between LoRa mote and server.
+  """
   use GenServer
+  require Logger
 
   def start_link(name) do
     GenServer.start_link(__MODULE__, nil, [name: name])
@@ -8,22 +12,92 @@ defmodule Handler do
   def handle_call(payload, _from, nil) do
     phy_payload = parse_packet(payload)
 
-    IO.puts  phy_payload.mhdr
-
     {:reply, "Packed handled", nil}
   end
 
-  defp parse_packet(payload) do
-    payload_size = byte_size(payload) - 5
+  @doc """
+  Multi-clause function to parse packets depending on their MHDR and Major.
+
+  """
+  defp parse_packet(<<
+    0x00    :: size(3), # MType Join Request
+    0x00    :: size(3), # RFU
+    0x00    :: size(2), # Major (0 = 1.0)
+    data    :: binary
+    >>) do 
+
+    payload_size = bit_size(data) - 32
+    Logger.info "this is a Join Request Packet"
 
     <<
-    mhdr    ::  size(1),
-    payload ::  size(payload_size),
-    mic     ::  size(4)
-    >> = payload
+      payload ::  size(payload_size), 
+      mic     ::  size(32)
+    >> = data
+    
+    %LoRaWAN.JoinRequest{
+      payload: payload,
+      mic: mic
+    }
+  end
 
-    %LoRaWAN.PHYPayload{
-      mhdr: mhdr,
+  defp parse_packet(<<
+    0x02    :: size(3), # MType Unconfirmed Data Up
+    0x00    :: size(3), # RFU
+    0x00    :: size(2), # Major (0 = 1.0)
+    data    :: binary
+    >>) do
+
+    payload_size = bit_size(data) - 32
+    Logger.info "this is an Unconfirmed Data Up Packet"
+
+    <<
+      payload ::  size(payload_size), 
+      mic     ::  size(32)
+    >> = data
+    
+    %LoRaWAN.UnconfirmedDataUp{
+      payload: payload,
+      mic: mic
+    }
+  end
+
+  defp parse_packet(<<
+    0x04    :: size(3), # MType Confirmed Data Up
+    0x00    :: size(3), # RFU
+    0x00    :: size(2), # Major (0 = 1.0)
+    data    :: binary
+    >>) do 
+
+    payload_size = bit_size(data) - 32
+    Logger.info "this is an Confirmed Data Up Packet"
+
+    <<
+      payload ::  size(payload_size),
+      mic     ::  size(32)
+    >> = data
+
+    %LoRaWAN.ConfirmedDataUp{
+      payload: payload,
+      mic: mic
+    }
+  end
+
+  defp parse_packet(<<
+    0x07    :: size(3), # MType Proprietary
+    0x00    :: size(3), # RFU
+    0x00    :: size(2), # Major (0 = 1.0)
+    data    :: binary
+    >>) do
+
+    payload_size = bit_size(data) - 32
+    Logger.info "this is a Proprietary Packet"
+
+    <<
+      payload ::  size(payload_size),
+      mic     ::  size(32)
+    >> = data
+
+    %LoRaWAN.Proprietary{
       payload: payload,
       mic: mic
     }
--- apps/lorawan/lib/lorawan/phy_payload.ex
+++ apps/lorawan/lib/lorawan/phy_payload.ex
@@ -1,7 +1,70 @@
-defmodule LoRaWAN.PHYPayload do
-   defstruct [
-    mhdr: nil,
+defmodule LoRaWAN.JoinRequest do
+  defstruct [
+    mtype: 0x00,
+    rfu: 0x00,
+    major: 0x00,
     payload: nil,
     mic: nil
   ]
 end
+
+defmodule LoRaWAN.JoinAccept do
+  defstruct [
+    mtype: 0x01,
+    rfu: 0x00,
+    major: 0x00,
+    payload: nil,
+    mic: nil
+  ]
+end
+
+defmodule LoRaWAN.UnconfirmedDataUp do
+  defstruct [
+    mtype: 0x02,
+    rfu: 0x00,
+    major: 0x00,
+    payload: nil,
+    mic: nil
+  ]
+end
+
+defmodule LoRaWAN.UnconfirmedDataDown do
+  defstruct [
+    mtype: 0x03,
+    rfu: 0x00,
+    major: 0x00,
+    payload: nil,
+    mic: nil
+  ]
+end
+
+defmodule LoRaWAN.ConfirmedDataUp do
+  defstruct [
+    mtype: 0x04,
+    rfu: 0x00,
+    major: 0x00,
+    payload: nil,
+    mic: nil
+  ]
+end
+
+defmodule LoRaWAN.ConfirmedDataDown do
+  defstruct [
+    mtype: 0x05,
+    rfu: 0x00,
+    major: 0x00,
+    payload: nil,
+    mic: nil
+  ]
+end
+
+defmodule LoRaWAN.Proprietary do
+  defstruct [
+    mtype: 0x07,
+    rfu: 0x00,
+    major: 0x00,
+    payload: nil,
+    mic: nil
+  ]
+end
+
