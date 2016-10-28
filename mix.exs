408b6474bbecec224aaaf25c25bcd47eb225ded6
tooreht <tooreht@gmail.com>
2016-10-28 21:45:11 +0200

    Add and configure distillery release tool

--- mix.exs
+++ mix.exs
@@ -21,6 +21,9 @@ defmodule Iotc.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    [{:ex_doc, "~> 0.14", only: :dev}]
+    [
+      {:ex_doc, "~> 0.14", only: :dev},
+      {:distillery, "~> 0.10"}
+    ]
   end
 end
