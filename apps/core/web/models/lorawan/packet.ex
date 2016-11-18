3fd0b20ec53ec0cd97e3212adcefecf4c9133761
tooreht <tooreht@gmail.com>
2016-11-18 23:53:09 +0100

    Scaffold LoRaWAN.Packet
    Using phoenix.gen.json task.

---
+++ apps/core/web/models/lorawan/packet.ex
@@ -0,0 +1,26 @@
+defmodule Core.LoRaWAN.Packet do
+  use Core.Web, :model
+
+  schema "lorawan_packets" do
+    field :number, :integer
+    field :type, :integer
+    field :frequency, :decimal
+    field :channel, :integer
+    field :modulation, :string
+    field :data_rate, :string
+    field :code_rate, :string
+    field :size, :integer
+    belongs_to :node, Core.LoRaWAN.Node
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:number, :type, :frequency, :channel, :modulation, :data_rate, :code_rate, :size])
+    |> validate_required([:number, :type, :frequency, :channel, :modulation, :data_rate, :code_rate, :size])
+  end
+end
