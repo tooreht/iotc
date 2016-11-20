9d6480b657e942e06fe391c69f558c2fecb257f7
tooreht <tooreht@gmail.com>
2016-11-20 13:30:12 +0100

    Limit columns for LoRaWAN.Node index view

--- apps/core/web/admin/lorawan/node.ex
+++ apps/core/web/admin/lorawan/node.ex
@@ -2,6 +2,18 @@ defmodule Core.ExAdmin.LoRaWAN.Node do
   use ExAdmin.Register
 
   register_resource Core.LoRaWAN.Node do
+    index do
+      selectable_column
 
+      column :id, fn node ->
+        raw Phoenix.HTML.Link.link(node.id, to: admin_resource_path(node, :show))
+      end
+      column :dev_eui
+      column :dev_addr
+      column :last_seen
+      column :application
+      column :user
+      actions
+    end
   end
 end
