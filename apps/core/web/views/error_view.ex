48c80317c6f765a1096d2dfd467bc58deff47ace
tooreht <tooreht@gmail.com>
2016-12-11 19:07:00 +0100

    Add json render fns in Core.ErrorView

--- apps/core/web/views/error_view.ex
+++ apps/core/web/views/error_view.ex
@@ -9,6 +9,18 @@ defmodule Core.ErrorView do
     "Internal server error"
   end
 
+  def render("401.json", _assigns) do
+    %{status: "Unauthorized"}
+  end
+
+  def render("404.json", _assigns) do
+    %{status: "Not Found"}
+  end
+
+  def render("500.json", _assigns) do
+    %{status: "Server Error"}
+  end
+
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
