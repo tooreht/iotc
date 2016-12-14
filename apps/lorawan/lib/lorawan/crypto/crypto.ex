7f92b4a1d7b57205f33ec122fda14e6679d25f39
Wallcreeper <simon.wittwer@outlook.com>
2016-12-14 20:14:46 +0100

    Implement new LoRaWAN structure in MIC
    - Implement structure changes from Frame to Packet
    - Fix bug where packets with counter > 255 don't pass MIC check
    - Implement tests to comply with the new structure

--- apps/lorawan/lib/lorawan/crypto/crypto.ex
+++ apps/lorawan/lib/lorawan/crypto/crypto.ex
@@ -3,8 +3,8 @@ defmodule LoRaWAN.Crypto do
   This module is responsible for the crypto part of LoRaWAN.
   """
   alias LoRaWAN.Crypto.AesCmac
-  alias LoRaWAN.Frame
-
+  alias LoRaWAN.Packet
+  require Logger
 
   @doc """
   Calculate and validate the MIC (Message Integrity Code).
@@ -32,42 +32,45 @@ defmodule LoRaWAN.Crypto do
                     |          |          |          |          |           |          |          |
                     -------------------------------------------------------------------------------
 
-  The direction field (Dir) is 0 for uplink frames and 1 for downlink frames.
+  The direction field (Dir) is 0 for uplink packets and 1 for downlink packets.
   """
-  def valid_mic?(frame) do
-    case frame.phy_payload.mhdr.m_type do
+  def valid_mic?(packet) do
+    case packet.mhdr.m_type do
       0x00 -> # Join Request
-        # MIC Check by APP KEY
-        false
-      0x02 -> # Unconfirmed Data Up
-        %Frame.Raw{phy_payload: _, mhdr: mhdr_raw, mac_payload: _, mic: mic_raw, fhdr: fhdr_raw, f_ctrl: _} = frame.raw
+        app_eui = packet.mac_payload.app_eui
+        dev_eui = packet.mac_payload.dev_eui
+        dev_nonce = packet.mac_payload.dev_nonce
 
-        fhdr = frame.phy_payload.mac_payload.fhdr
-        f_port = frame.phy_payload.mac_payload.f_port
-        frm_payload = frame.phy_payload.mac_payload.frm_payload
+        app_key = get_app_key(app_eui, dev_eui)
+
+        mhdr_raw = packet.mhdr.raw
+        mic_raw = packet.mic
+
+        msg = mhdr_raw <> app_eui <> dev_eui <> dev_nonce
+        cmac = AesCmac.aes_cmac(app_key, msg)
+        <<computed_mic::binary-size(4), _::binary>> = cmac
+
+        computed_mic == mic_raw
+      n when n in [0x02, 0x04] -> # Unconfirmed Data Up
+        mhdr_raw = packet.mhdr.raw
+        mic_raw = packet.mic
+        fhdr_raw = packet.mac_payload.fhdr.raw
+
+        fhdr = packet.mac_payload.fhdr
+        f_port = packet.mac_payload.f_port
+        frm_payload = packet.mac_payload.frm_payload
 
         msg = IO.iodata_to_binary [mhdr_raw, fhdr_raw, f_port, frm_payload]
         b0 = IO.iodata_to_binary [0x49, 0, 0, 0, 0, 0, fhdr.dev_addr, <<fhdr.f_cnt::little-unsigned-integer-size(32)>>, 0, <<byte_size(msg)::8>>]
-        IO.puts("DevAddr #{inspect(fhdr.dev_addr)}")
-        nws_key = get_nws_key(fhdr.dev_addr)        
 
-        IO.puts("NWS Key #{inspect(nws_key)}")
-        IO.puts("MSG #{inspect(msg)}")
-        IO.puts("B0 #{inspect(b0)}")
+        nws_key = get_nws_key(fhdr.dev_addr)        
 
         cmac = AesCmac.aes_cmac(nws_key, IO.iodata_to_binary [b0, msg])
         <<computed_mic::binary-size(4), _::binary>> = cmac
 
-        IO.puts("CMAC #{inspect(cmac)}")
-        IO.puts("Computed MIC #{inspect(computed_mic)}")
-        IO.puts("MIC #{inspect(mic_raw)}")
-
         computed_mic == mic_raw
-      0x04 -> # Confirmed Data Up
-        nws_key = get_nws_key(frame.phy_payload.mac_payload.fhdr.dev_addr)
-        IO.puts(inspect nws_key)
-        false
       _ ->
+        Logger.warn "no valid m_type"
         false
     end
   end
@@ -79,15 +82,23 @@ defmodule LoRaWAN.Crypto do
         <<131, 132, 1, 172,  17, 228, 225, 223, 76, 106, 213, 106, 7,  71, 210, 196>>
       <<208, 246, 125, 29>> ->
         <<121, 91, 177, 245, 56, 223, 79, 142, 120, 116, 172, 170, 82, 95, 205, 204>>
+      <<70, 161, 210, 121>> ->
+        <<131, 132, 1, 172, 17, 228, 225, 223, 76, 106, 213, 106, 7, 71, 210, 196>>
       _ ->
+        Logger.warn "No NwkSKey found!"
         <<0x00000000000000000000000000000000::128>>
     end
   end
 
   defp get_app_key(app_eui, dev_eui) do
     # TODO: lookup app_eui in DB and get app_key
-    #case app_eui, dev_eui do
-    #end
+    case {app_eui, dev_eui} do
+      {<<54, 14, 0, 208, 126, 213, 179, 112>>, <<139, 119, 102, 95, 78, 61, 43, 42>>} ->
+        <<191, 185, 219, 229, 78, 139, 209, 131, 24, 59, 112, 168, 106, 103, 175, 73>>
+      _ ->
+        Logger.warn "No combination of AppEUI + DevEUI found -> no AppKey Found!"
+        <<0x00000000000000000000000000000000::128>>
+    end
   end
 
 end
--- apps/lorawan/test/lorawan/crypto/crypto_test.exs
+++ apps/lorawan/test/lorawan/crypto/crypto_test.exs
@@ -3,32 +3,23 @@ defmodule LoRaWAN.Crypto.CryptoTest do
   alias LoRaWAN.Crypto.AesCmac
 
 
-  test "test MIC" do
+  test "test Data MIC" do
     nwsk = <<121, 91, 177, 245, 56, 223, 79, 142, 120, 116, 172, 170, 82, 95, 205, 204>>
 
     data = <<64, 208, 246, 125, 29, 0, 0, 0, 3, 172, 229, 114, 233, 126, 95>>
-    IO.puts "Data #{inspect(data)}"
+
     <<mhdr::binary-size(1), rest::binary>> = data
-    IO.puts "MHDR #{inspect(mhdr)}"
+
     mac_payload_len = byte_size(rest) - 4
-    IO.puts "MAC Payload Length #{inspect(mac_payload_len)}"
+
     <<mac_payload::binary-size(mac_payload_len), mic::binary>> = rest
-    IO.puts "MAC Payload #{inspect(mac_payload)}"
-    IO.puts "MIC #{inspect(mic)}"
+
 
     <<dev_addr::binary-size(4), fctrl::binary-size(1), fcnt::little-unsigned-integer-size(16), rest::binary>> = mac_payload
 
-    IO.puts "DevAddr #{inspect(String.reverse(dev_addr))}"
-    IO.puts "FCtrl #{inspect(fctrl)}"
-    IO.puts "FCnt #{inspect(fcnt)}"
+    <<_::4, fopts_len::integer-size(4)>> = fctrl
 
-    <<adr::1, adr_ack_req::1, ack::1, rfu::1, fopts_len::integer-size(4)>> = fctrl
 
-    IO.puts "ADR #{inspect(adr)}"
-    IO.puts "ADR_ACK_Req #{inspect(adr_ack_req)}"
-    IO.puts "ACK #{inspect(ack)}"
-    IO.puts "RFU #{inspect(rfu)}"
-    IO.puts "FOptsLen #{inspect(fopts_len)}"
 
     {fopts, fport, frm_payload} = case fopts_len do
       0 ->
@@ -36,24 +27,14 @@ defmodule LoRaWAN.Crypto.CryptoTest do
         {0, fport, frm_payload}
       _ -> {0, 0, 0}
     end
-    IO.puts "FPort #{inspect(fport)}"
-    IO.puts "FRMPayload #{inspect(frm_payload)}"
 
     fhdr = IO.iodata_to_binary([dev_addr, fctrl, fcnt, fopts])
-    IO.puts "FHDR #{inspect(fhdr)}"
 
     msg = IO.iodata_to_binary [mhdr, fhdr, fport, frm_payload]
     b0 = IO.iodata_to_binary [0x49, 0, 0, 0, 0, 0, dev_addr, <<fcnt::little-unsigned-integer-size(32)>>, 0, <<byte_size(msg)::8>>]
 
-    IO.puts("MSG #{inspect(msg)}")
-    IO.puts("B0 #{inspect(b0)}")
-
     cmac = AesCmac.aes_cmac(nwsk, IO.iodata_to_binary [b0, msg])
     <<computed_mic::binary-size(4), _::binary>> = cmac
-
-    IO.puts("CMAC #{inspect(cmac)}")
-    IO.puts("Computed MIC #{inspect(computed_mic)}")
-    IO.puts("MIC #{inspect(mic)}")
 
     assert computed_mic == mic
   end
