c55f39db53ef16628e51c515360887c9d5bf0268
Wallcreeper <simon.wittwer@outlook.com>
2016-12-19 11:09:57 +0100

    Do cosmetic update on decoder_test

--- apps/lorawan/test/lorawan/decoder_test.exs
+++ apps/lorawan/test/lorawan/decoder_test.exs
@@ -2,7 +2,7 @@ defmodule LoRaWAN.DecoderTest do
   use ExUnit.Case, async: true
   alias LoRaWAN.Packet
 
-  test "It should extract an UnconfirmedDataUp packet." do
+  test "It should extract an Data Message." do
     # Origin Base64 Message:  QOvHuxwAAAADifcrgYr0
     # HEX:                    0x40 0xEB 0xC7 0xBB 0x1C 0x00 0x00 0x00 0x03 0x89 0xF7 0x2B 0x81 0x8A 0xF4
     # Binary:                 <<64, 235, 199, 187, 28, 0, 0, 0, 3, 137, 247, 43, 129, 138, 244>>
@@ -23,14 +23,14 @@ defmodule LoRaWAN.DecoderTest do
         m_type: 0x02,
         rfu: 0,
         major: 0,
-        raw: "@"
+        raw: <<64>>
       },
       mac_payload: %Packet.DataMACPayload{
         fhdr: %Packet.FHDR{
             dev_addr: <<235, 199, 187, 28>>,
             f_ctrl: fCtrl,
             f_cnt: 0,
-            f_opts: "",
+            f_opts: <<0::0>>,
             raw: <<235, 199, 187, 28, 0, 0, 0>>
           },
         f_port: <<3>>,
@@ -42,7 +42,7 @@ defmodule LoRaWAN.DecoderTest do
     }
   end
 
-  test "It should extract an JoinRequest packet." do
+  test "It should extract an Join Message." do
     # Origin Base64 Message: ADYOANB+1bNwi3dmX049Kyo9EiNeX3o=
     # HEX:                   0x00 0x36 0x0E 0x00 0xD0 0x7E 0xD5 0xB3 0x70 0x8B 0x77 0x66 0x5F 0x4E 0x3D 0x2B 0x2A 0x3D 0x12 0x23 0x5E 0x5F 0x7A
     # Binary:                <<0, 54, 14, 0, 208, 126, 213, 179, 112, 139, 119, 102, 95, 78, 61, 43, 42, 61, 18, 35, 94, 95, 122>>
@@ -65,13 +65,13 @@ defmodule LoRaWAN.DecoderTest do
     assert packet.mic == "#^_z"
   end
 
-  test "It should extract an UnconfirmedDataUp packet (new)." do
+  test "It should extract a Data Message (new)." do
     phy_payload = <<64, 208, 246, 125, 29, 0, 0, 0, 5, 172, 229, 114, 233, 126, 95>>
     packet = LoRaWAN.Decoder.decode phy_payload
 
     assert packet.mhdr.m_type == 2
     assert packet.mhdr.major == 0
-    assert packet.mhdr.raw == "@"
+    assert packet.mhdr.raw == <<64>>
     assert packet.mhdr.rfu == 0
 
     assert packet.mac_payload.fhdr.dev_addr == <<208, 246, 125, 29>>
@@ -81,7 +81,7 @@ defmodule LoRaWAN.DecoderTest do
     assert packet.mac_payload.fhdr.f_ctrl.f_opts_len == 0
     assert packet.mac_payload.fhdr.f_ctrl.raw == <<0>>
     assert packet.mac_payload.fhdr.f_cnt == 0
-    assert packet.mac_payload.fhdr.f_opts == ""
+    assert packet.mac_payload.fhdr.f_opts == <<0::0>>
     assert packet.mac_payload.fhdr.raw == <<208, 246, 125, 29, 0, 0, 0>>
 
     assert packet.mac_payload.f_port == <<5>>
@@ -90,5 +90,34 @@ defmodule LoRaWAN.DecoderTest do
 
     assert packet.mic == <<114, 233, 126, 95>>
     assert packet.raw == <<64, 208, 246, 125, 29, 0, 0, 0, 5, 172, 229, 114, 233, 126, 95>>
+  end
+
+  test "It should extract a Data Message" do
+    phy_payload = <<64, 221, 83, 103, 169, 130, 138, 6, 3, 7, 20, 44, 180, 122, 249, 213, 75, 180, 131, 118, 203, 232, 90, 191, 46>>
+    packet = LoRaWAN.Decoder.decode phy_payload
+
+    assert packet.mhdr.m_type == 2
+    assert packet.mhdr.major == 0
+    assert packet.mhdr.raw == <<64>>
+    assert packet.mhdr.rfu == 0
+
+    assert packet.mac_payload.fhdr.dev_addr == <<221, 83, 103, 169>>
+    assert packet.mac_payload.fhdr.f_ctrl.adr == 1
+    assert packet.mac_payload.fhdr.f_ctrl.adr_ack_req == 0
+    assert packet.mac_payload.fhdr.f_ctrl.ack == 0
+    assert packet.mac_payload.fhdr.f_ctrl.f_opts_len == 2
+    assert packet.mac_payload.fhdr.f_ctrl.raw == <<130>>
+    assert packet.mac_payload.fhdr.f_cnt == 1674
+    assert packet.mac_payload.fhdr.f_opts == <<3, 7>>
+    assert packet.mac_payload.fhdr.raw == <<221, 83, 103, 169, 130, 138, 6, 3, 7>>
+
+    assert packet.mac_payload.f_port == <<20>>
+    assert packet.mac_payload.frm_payload == <<44, 180, 122, 249, 213, 75, 180, 131, 118, 203>>
+    assert packet.mac_payload.raw == <<221, 83, 103, 169, 130, 138, 6, 3, 7, 20, 44, 180, 122, 249, 213, 75, 180, 131, 118, 203>>
+
+    assert packet.mic == <<232, 90, 191, 46>>
+    assert packet.raw == <<64, 221, 83, 103, 169, 130, 138, 6, 3, 7, 20, 44, 180, 122, 249, 213, 75, 180, 131, 118, 203, 232, 90, 191, 46>>
+
+
   end
 end
