a8667ff606236b806c6f14f8a06e86f6389bcaf7
tooreht <tooreht@gmail.com>
2016-12-28 22:09:34 +0100

    Update seeds in core and appsrv app

--- apps/appsrv/priv/repo/seeds.exs
+++ apps/appsrv/priv/repo/seeds.exs
@@ -10,11 +10,20 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+Appsrv.Repo.delete_all Appsrv.LoRaWAN.Node
 Appsrv.Repo.delete_all Appsrv.LoRaWAN.Application
 Appsrv.Repo.delete_all Appsrv.User
 
 user = Appsrv.Repo.insert! Appsrv.User.changeset(%Appsrv.User{}, %{name: "Admin", email: "admin@example.net", username: "admin", password: "secret"})
 
-app = Appsrv.Repo.insert! %Appsrv.LoRaWAN.Application{app_eui: "70B3D57ED0000E36", app_root_key: "A1B2C3D4E5F60000", name: "AlloAllo", user_id: user.id}
+tresh_app = Appsrv.Repo.insert! %Appsrv.LoRaWAN.Application{app_eui: "70B3D57ED000177C", app_root_key: "A1B2C3D4E5F60000", name: "Tresh", user_id: user.id}
+allo_app = Appsrv.Repo.insert! %Appsrv.LoRaWAN.Application{app_eui: "70B3D57ED0000E36", app_root_key: "A1B2C3D4E5F60001", name: "AlloAllo", user_id: user.id}
+other_app = Appsrv.Repo.insert! %Appsrv.LoRaWAN.Application{app_eui: "AAAAAAAAD0111E11", app_root_key: "A1B2C3D4E5F60002", name: "Other", user_id: user.id}
 
-node = Appsrv.Repo.insert! %Appsrv.LoRaWAN.Node{name: "SuperNode", dev_eui: "2A2B3D4E5F66778A", dev_addr: "1CF3BA77", app_key: "9056E66E691EC92131DC6A16DB533C81", app_s_key: "8997C1DDA114D001B15E6CB58DB538A6", nwk_s_key: "54C90E4A5174CBC18423213153B97A62", relax_fcnt: true, rx1_dr_offset: 42, rx2_dr: 42, rx_delay: 42, rx_window: 42, application_id: app.id, user_id: user.id}
+Appsrv.Repo.insert! %Appsrv.LoRaWAN.Node{name: "0000000079D2A146", dev_eui: "0000000079D2A146", dev_addr: "79D2A146", app_key: "00000000000000000000000000000000", app_s_key: "D4A10CC6C632A264025FC3414E709F47", nwk_s_key: "838401AC11E4E1DF4C6AD56A0747D2C4", relax_fcnt: true, rx1_dr_offset: 42, rx2_dr: 42, rx_delay: 42, rx_window: 42, application_id: allo_app.id, user_id: user.id}
+Appsrv.Repo.insert! %Appsrv.LoRaWAN.Node{name: "00000000054aeae8", dev_eui: "00000000054AEAE8", dev_addr: "2601135F", app_key: "00000000000000000000000000000000", app_s_key: "F29AE50B8B907243EC3055A9D75479B8", nwk_s_key: "DCBB2BDE00339EBA7D288FF95FF419A2", relax_fcnt: true, rx1_dr_offset: 42, rx2_dr: 42, rx_delay: 42, rx_window: 42, application_id: tresh_app.id, user_id: user.id}
+Appsrv.Repo.insert! %Appsrv.LoRaWAN.Node{name: "SuperNode1", dev_eui: "0000000011111111", dev_addr: "1D7DF6D0", app_key: "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", app_s_key: "8997C1DDA114D001B15E6CB58DB538A6", nwk_s_key: "CCCD5F52AAAC74788E4FDF38F5B15B79", relax_fcnt: true, rx1_dr_offset: 42, rx2_dr: 42, rx_delay: 42, rx_window: 42, application_id: other_app.id, user_id: user.id}
+Appsrv.Repo.insert! %Appsrv.LoRaWAN.Node{name: "2A2B3D4E5F66778B", dev_eui: "2A2B3D4E5F66778B", dev_addr: "01010101", app_key: "BFB9DBE54E8BD183183B70A86A67AF49", app_s_key: "27D96E07661EA37AA4FCEC6BD99B112A", nwk_s_key: "E3A71DA6B2F1D2F7422ACE702FF56E74", relax_fcnt: true, rx1_dr_offset: 42, rx2_dr: 42, rx_delay: 42, rx_window: 42, application_id: allo_app.id, user_id: user.id}
+Appsrv.Repo.insert! %Appsrv.LoRaWAN.Node{name: "2A2B3D4E5F66778A", dev_eui: "2A2B3D4E5F66778A", dev_addr: "00000002", app_key: "9056E66E691EC92131DC6A16DB533C81", app_s_key: "86F09F4A256B75C42384B5724F210D00", nwk_s_key: "0D49F5CED7097CD16CBD67FD3E2BCF5D", relax_fcnt: true, rx1_dr_offset: 42, rx2_dr: 42, rx_delay: 42, rx_window: 42, application_id: allo_app.id, user_id: user.id}
+Appsrv.Repo.insert! %Appsrv.LoRaWAN.Node{name: "SuperNode2", dev_eui: "0101010101010101", dev_addr: "00000002", app_key: "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF", app_s_key: "8997C1DDA114D001B15E6CB58DB538A6", nwk_s_key: "44C01B3B2171BAC334123231BA5221DF", relax_fcnt: true, rx1_dr_offset: 42, rx2_dr: 42, rx_delay: 42, rx_window: 42, application_id: allo_app.id, user_id: user.id}
+Appsrv.Repo.insert! %Appsrv.LoRaWAN.Node{name: "marcs_abp", dev_eui: "0025EB4E5983C7BF", dev_addr: "26011C47", app_key: "00000000000000000000000000000000", app_s_key: "3C93396D451E83073F294EB246F08CD7", nwk_s_key: "01B9A4BEE9FBAE7C14CD3BBDFD4580E0", relax_fcnt: true, rx1_dr_offset: 42, rx2_dr: 42, rx_delay: 42, rx_window: 42, application_id: tresh_app.id, user_id: user.id}
--- apps/core/priv/repo/seeds.exs
+++ apps/core/priv/repo/seeds.exs
@@ -10,6 +10,7 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+Core.Repo.delete_all Core.LoRaWAN.Gateway.Statistics
 Core.Repo.delete_all Core.LoRaWAN.Gateway
 Core.Repo.delete_all Core.LoRaWAN.Packet
 Core.Repo.delete_all Core.LoRaWAN.Node
@@ -19,22 +20,24 @@ Core.Repo.delete_all Core.User
 
 user = Core.Repo.insert! Core.User.changeset(%Core.User{}, %{name: "Admin", email: "admin@example.net", username: "admin", password: "secret"})
  
-tresh_application = Core.Repo.insert! %Core.LoRaWAN.Application{app_eui: "70B3D57ED000177C", user_id: user.id}
-allo_application = Core.Repo.insert! %Core.LoRaWAN.Application{app_eui: "70B3D57ED0000E36", user_id: user.id}
-other_application = Core.Repo.insert! %Core.LoRaWAN.Application{app_eui: "AAAAAAAAD0111E11", user_id: user.id}
+tresh_app = Core.Repo.insert! %Core.LoRaWAN.Application{app_eui: "70B3D57ED000177C", user_id: user.id}
+allo_app = Core.Repo.insert! %Core.LoRaWAN.Application{app_eui: "70B3D57ED0000E36", user_id: user.id}
+other_app = Core.Repo.insert! %Core.LoRaWAN.Application{app_eui: "AAAAAAAAD0111E11", user_id: user.id}
  
 dev_addr70 = Core.Repo.insert! %Core.LoRaWAN.DeviceAddress{dev_addr: "79D2A146", last_assigned: Ecto.DateTime.utc}
 dev_addr232 = Core.Repo.insert! %Core.LoRaWAN.DeviceAddress{dev_addr: "2601135F", last_assigned: Ecto.DateTime.utc}
 dev_addr208 = Core.Repo.insert! %Core.LoRaWAN.DeviceAddress{dev_addr: "1D7DF6D0", last_assigned: Ecto.DateTime.utc}
 dev_addr1 = Core.Repo.insert! %Core.LoRaWAN.DeviceAddress{dev_addr: "01010101", last_assigned: Ecto.DateTime.utc}
 dev_addr2 = Core.Repo.insert! %Core.LoRaWAN.DeviceAddress{dev_addr: "00000002", last_assigned: Ecto.DateTime.utc}
+dev_addr3 = Core.Repo.insert! %Core.LoRaWAN.DeviceAddress{dev_addr: "26011C47", last_assigned: Ecto.DateTime.utc}
 
-Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "0000000079D2A146", device_address_id: dev_addr70.id, nwk_s_key: "838401AC11E4E1DF4C6AD56A0747D2C4", application_id: allo_application.id, user_id: user.id}
-Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "00000000054AEAE8", device_address_id: dev_addr232.id, nwk_s_key: "7A395AB2BE17D4B5305FE913ACBF2AA1", application_id: tresh_application.id, user_id: user.id}
-Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "0000000011111111", device_address_id: dev_addr208.id, nwk_s_key: "CCCD5F52AAAC74788E4FDF38F5B15B79", application_id: other_application.id, user_id: user.id}
-Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "2A2B3D4E5F66778B", device_address_id: dev_addr1.id, nwk_s_key: "DF40926674B8D287C6F9BF581CF11E11", application_id: allo_application.id, user_id: user.id}
-Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "2A2B3D4E5F66778A", device_address_id: dev_addr2.id, nwk_s_key: "54C90E4A5174CBC18423213153B97A62", application_id: allo_application.id, user_id: user.id}
-Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "0101010101010101", device_address_id: dev_addr2.id, nwk_s_key: "44C01B3B2171BAC334123231BA5221DF", application_id: allo_application.id, user_id: user.id}
+Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "0000000079D2A146", device_address_id: dev_addr70.id, nwk_s_key: "838401AC11E4E1DF4C6AD56A0747D2C4", application_id: allo_app.id, user_id: user.id}
+Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "00000000054AEAE8", device_address_id: dev_addr232.id, nwk_s_key: "DCBB2BDE00339EBA7D288FF95FF419A2", application_id: tresh_app.id, user_id: user.id}
+Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "0000000011111111", device_address_id: dev_addr208.id, nwk_s_key: "CCCD5F52AAAC74788E4FDF38F5B15B79", application_id: other_app.id, user_id: user.id}
+Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "2A2B3D4E5F66778B", device_address_id: dev_addr1.id, nwk_s_key: "E3A71DA6B2F1D2F7422ACE702FF56E74", application_id: allo_app.id, user_id: user.id}
+Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "2A2B3D4E5F66778A", device_address_id: dev_addr2.id, nwk_s_key: "0D49F5CED7097CD16CBD67FD3E2BCF5D", application_id: allo_app.id, user_id: user.id}
+Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "0101010101010101", device_address_id: dev_addr2.id, nwk_s_key: "44C01B3B2171BAC334123231BA5221DF", application_id: allo_app.id, user_id: user.id}
+Core.Repo.insert! %Core.LoRaWAN.Node{dev_eui: "0025EB4E5983C7BF", device_address_id: dev_addr3.id, nwk_s_key: "01B9A4BEE9FBAE7C14CD3BBDFD4580E0", application_id: tresh_app.id, user_id: user.id}
 
 Core.Repo.insert! %Core.LoRaWAN.Gateway{gw_eui: "B827EBFFFE7FE413", adapter: "Semtech", user_id: user.id, latitude: 0, longitude: 1, altitude: 2}
 Core.Repo.insert! %Core.LoRaWAN.Gateway{gw_eui: "B827EBFFFE8AA02D", adapter: "Semtech", user_id: user.id, latitude: 3, longitude: 4, altitude: 5}
