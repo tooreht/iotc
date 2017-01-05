e3e37285e7d371e9083b43a5dffafb380aa57aa0
tooreht <tooreht@gmail.com>
2017-01-05 22:47:04 +0100

    Implement Adapter behaviour in SIOT Adapter

--- apps/appsrv/lib/appsrv/adapters/siot.ex
+++ apps/appsrv/lib/appsrv/adapters/siot.ex
@@ -5,6 +5,8 @@ defmodule Appsrv.Adapters.SIOT do
   ## Transport
   The data is exchanged over [MQTT](http://mqtt.org/).
   """
+  @behaviour Appsrv.Adapter
+
   use GenMQTT
 
   require Logger
@@ -15,24 +17,79 @@ defmodule Appsrv.Adapters.SIOT do
     GenMQTT.start_link(__MODULE__, :ok, opts)
   end
 
-  def on_connect(state) do
-    # TODO: Add subscription through SIOT manifest
-    :ok = GenMQTT.subscribe(self, "rx/app/+/node/+", 0)
-    {:ok, state}
-  end
+  # def on_connect(state) do
+  #   :ok = GenMQTT.subscribe(self, "", 0)
+  #   {:ok, state}
+  # end
 
-  def on_publish(["rx", "app", app_eui, "node", dev_eui], message, state) do
-    # TODO: Adapt to SIOT specific topics
-    Logger.info "(SIOT) Receive unencrypted data on MQTT topic: #{inspect(message)} on topic rx/app/#{app_eui}/node/#{dev_eui}"
-    Appsrv.LoRaWAN.Handler.send(Appsrv.LoRaWAN.Handler, message, dev_eui)
-    {:ok, state}
-  end
+  # def on_publish(["siot", "?", dev_eui], message, state) do
+  #   Logger.info "(SIOT) Receive unencrypted data #{inspect(message)} on MQTT topic: ?"
+  #   Appsrv.LoRaWAN.Handler.send(Appsrv.LoRaWAN.Handler, message, dev_eui)
+  #   {:ok, state}
+  # end
 
   # Adapter behaviour
 
   def send(bytes, node) do
-    # TODO: Adapt to SIOT specific topics
-    Logger.info "(SIOT) Publish decrypted data #{inspect(bytes)} on MQTT topic tx/app/#{node.application.app_eui}/node/#{node.dev_eui}"
-    GenMQTT.publish(__MODULE__, "tx/app/#{node.application.app_eui}/node/#{node.dev_eui}", bytes, 0)
+    dst = Enum.at(bytes, 0) |> to_string
+    soc = Enum.at(bytes, 1) |> to_string
+    tmp = Enum.at(bytes, 2) |> to_string
+
+    dst_topic = "siot/DAT/#{Application.get_env(:appsrv, __MODULE__)[:licence]}/#{node.dev_eui}-dst"
+    soc_topic = "siot/DAT/#{Application.get_env(:appsrv, __MODULE__)[:licence]}/#{node.dev_eui}-soc"
+    tmp_topic = "siot/DAT/#{Application.get_env(:appsrv, __MODULE__)[:licence]}/#{node.dev_eui}-tmp"
+
+    Logger.info "(SIOT) Publish decrypted data #{dst} on MQTT topic #{dst_topic}"
+    Logger.info "(SIOT) Publish decrypted data #{soc} on MQTT topic #{soc_topic}"
+    Logger.info "(SIOT) Publish decrypted data #{tmp} on MQTT topic #{tmp_topic}"
+
+    GenMQTT.publish(__MODULE__, dst_topic, dst, 0)
+    GenMQTT.publish(__MODULE__, soc_topic, soc, 0)
+    GenMQTT.publish(__MODULE__, tmp_topic, tmp, 0)
+  end
+
+  def register(application, node) do
+    Logger.info "(SIOT) Register sensor values for node #{node.dev_eui}"
+    # TODO: Let the user define the SIOT manifestation of his nodes
+    dst_manifest = %{
+      name: "Distance",
+      type: "sensor",
+      zone: %{
+        name: "#{application.name}",
+        guid: "#{application.app_eui}"
+      },
+      description: "Distance (cm)",
+      valueType: "int"
+    }
+    soc_manifest = %{
+      name: "State of charge",
+      type: "sensor",
+      zone: %{
+        name: "#{application.name}",
+        guid: "#{application.app_eui}"
+      },
+      description: "SOC (%)",
+      valueType: "int"
+    }
+    tmp_manifest = %{
+      name: "Temperature",
+      type: "sensor",
+      zone: %{
+        name: "#{application.name}",
+        guid: "#{application.app_eui}"
+      },
+      description: "Temperature (°C)",
+      valueType: "int"
+    }
+
+    # Manifest each sensor value
+    GenMQTT.publish(__MODULE__, "siot/MNF/#{Application.get_env(:appsrv, __MODULE__)[:licence]}/#{node.dev_eui}-dst", Poison.encode!(dst_manifest), 0)
+    GenMQTT.publish(__MODULE__, "siot/MNF/#{Application.get_env(:appsrv, __MODULE__)[:licence]}/#{node.dev_eui}-soc", Poison.encode!(soc_manifest), 0)
+    GenMQTT.publish(__MODULE__, "siot/MNF/#{Application.get_env(:appsrv, __MODULE__)[:licence]}/#{node.dev_eui}-tmp", Poison.encode!(tmp_manifest), 0)
+
+    # Configure that the data should be stored in the database, not just in memory
+    GenMQTT.publish(__MODULE__, "siot/CNF/#{Application.get_env(:appsrv, __MODULE__)[:licence]}/#{node.dev_eui}-dst", Poison.encode!(%{storage: "db"}), 0)
+    GenMQTT.publish(__MODULE__, "siot/CNF/#{Application.get_env(:appsrv, __MODULE__)[:licence]}/#{node.dev_eui}-soc", Poison.encode!(%{storage: "db"}), 0)
+    GenMQTT.publish(__MODULE__, "siot/CNF/#{Application.get_env(:appsrv, __MODULE__)[:licence]}/#{node.dev_eui}-tmp", Poison.encode!(%{storage: "db"}), 0)
   end
 end
