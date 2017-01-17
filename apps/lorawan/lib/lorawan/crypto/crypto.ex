85ada5296cceadf2e9590f3201bb31163f7c5bee
tooreht <tooreht@gmail.com>
2017-01-17 19:51:52 +0100

    Move AppSrv.LoRaWAN.Crypto to LoRaWAN.Crypto

--- apps/lorawan/lib/lorawan/crypto/crypto.ex
+++ apps/lorawan/lib/lorawan/crypto/crypto.ex
@@ -2,10 +2,9 @@ defmodule LoRaWAN.Crypto do
   @moduledoc """
   This module is responsible for the crypto part of LoRaWAN.
   """
+  use Bitwise
+
   alias LoRaWAN.Crypto.AesCmac
-  alias NwkSrv.Storage
-  alias NwkSrv.Storage.Utils
-  require Logger
 
   @doc """
   Calculate and validate the MIC (Message Integrity Code).
@@ -35,57 +34,37 @@ defmodule LoRaWAN.Crypto do
 
   The direction field (Dir) is 0 for uplink packets and 1 for downlink packets.
   """
-  def valid_mic(packet) do
-    case packet.mhdr.m_type do
-      0x00 -> # Join Request
-        app_eui = packet.mac_payload.app_eui
-        dev_eui = packet.mac_payload.dev_eui
-        dev_nonce = packet.mac_payload.dev_nonce
+  def valid_mic(:join, {packet, app_key}) do
+    msg = IO.iodata_to_binary [
+      packet.mhdr.raw,
+      packet.mac_payload.app_eui,
+      packet.mac_payload.dev_eui,
+      packet.mac_payload.dev_nonce
+    ]
+    cmac = AesCmac.aes_cmac(app_key, msg)
+    computed_mic = :erlang.binary_part(cmac, 0, 4)
 
-        app_key = get_app_key(app_eui, dev_eui)
-
-        mhdr_raw = packet.mhdr.raw
-        mic_raw = packet.mic
-
-        msg = IO.iodata_to_binary [mhdr_raw, app_eui, dev_eui, dev_nonce]
-        cmac = AesCmac.aes_cmac(app_key, msg)
-        computed_mic = :erlang.binary_part(cmac, 0, 4)
-
-        {computed_mic == mic_raw, packet}
-      n when n in [0x02, 0x04] -> # Unconfirmed Data Up
-        mhdr_raw = packet.mhdr.raw
-        mic_raw = packet.mic
-        fhdr_raw = packet.mac_payload.fhdr.raw
-
-        fhdr = packet.mac_payload.fhdr
-        f_port = packet.mac_payload.f_port
-        frm_payload = packet.mac_payload.frm_payload
-
-        msg = IO.iodata_to_binary [mhdr_raw, fhdr_raw, f_port, frm_payload]
-        b0 = IO.iodata_to_binary [0x49, 0, 0, 0, 0, 0, fhdr.dev_addr, <<fhdr.f_cnt::little-unsigned-integer-size(32)>>, 0, <<byte_size(msg)::8>>]
-        data = IO.iodata_to_binary [b0, msg]
-
-        nodes = get_nodes(fhdr.dev_addr, fhdr.f_cnt)
-
-        try_mic(nodes, data, mic_raw)
-      _ ->
-        Logger.warn "no valid m_type"
-        {false, nil}
-    end
+    {computed_mic == packet.mic, packet}
   end
 
-  defp get_nodes(rev_dev_addr, f_cnt) do
-    dev_addr = Utils.rev_bytes_to_base16(rev_dev_addr)
-    nodes = Storage.DB.LoRaWAN.Node.get_nodes(%{dev_addr: dev_addr, f_cnt: f_cnt})
-    if nodes == [] do
-      Logger.warn "no device with dev_addr " <> inspect(dev_addr) <> " found!"
-      nodes
-    else
-      nodes
-    end
+  def valid_mic(:data, {packet, nodes}) do
+    msg = IO.iodata_to_binary [
+      packet.mhdr.raw,
+      packet.mac_payload.fhdr.raw,
+      packet.mac_payload.f_port,
+      packet.mac_payload.frm_payload
+    ]
+    b0 = IO.iodata_to_binary [
+      0x49, 0, 0, 0, 0, 0,
+      packet.mac_payload.fhdr.dev_addr,
+      <<packet.mac_payload.fhdr.f_cnt::little-unsigned-integer-size(32)>>,
+      0,
+      <<byte_size(msg)::8>>
+    ]
+    try_mic(nodes, IO.iodata_to_binary([b0, msg]), packet.mic)
   end
 
-  def try_mic([node | tail], data, mic) do
+  defp try_mic([node | tail], data, mic) do
     cmac = AesCmac.aes_cmac(Base.decode16!(node.nwk_s_key), data)
     computed_mic = :erlang.binary_part(cmac, 0, 4)
     if computed_mic == mic do
@@ -95,19 +74,69 @@ defmodule LoRaWAN.Crypto do
     end
   end
 
-  def try_mic([], data, mic) do
+  defp try_mic([], data, mic) do
     {false, nil}
   end
 
-  defp get_app_key(app_eui, dev_eui) do
-    # TODO: lookup app_eui in DB and get app_key
-    case {app_eui, dev_eui} do
-      {<<54, 14, 0, 208, 126, 213, 179, 112>>, <<139, 119, 102, 95, 78, 61, 43, 42>>} ->
-        <<191, 185, 219, 229, 78, 139, 209, 131, 24, 59, 112, 168, 106, 103, 175, 73>>
-      _ ->
-        Logger.warn "No combination of AppEUI + DevEUI found -> no AppKey Found!"
-        <<0x00000000000000000000000000000000::128>>
+  @doc """
+  The key K used depends on the FPort of the data message:
+
+        FPort     |       K
+  ---------------------------------
+  |       0       |   nwk_s_key   |
+  ---------------------------------
+  |     1..255    |   app_s_key   |
+  ---------------------------------
+
+  The fields encrypted are:
+
+      pld = FRMPayload
+
+  For each data message, the algorithm defines a sequence of Blocks Ai for i = 1..k with k = ceil(len(pld) / 16):
+
+                    -------------------------------------------------------------------------------
+       Size (bytes) |     1    |    4     |    1     |    4     |    4      |    1     |    1     |
+      --------------|----------|----------|----------|----------|-----------|----------|-----------
+                 Ai |   0x01   | 4 x 0x00 |    Dir   | DevAddr  | FCntUp or |   0x00   |    i     |
+                    |          |          |          |          | FCntDown  |          |          |
+                    |          |          |          |          |           |          |          |
+                    -------------------------------------------------------------------------------
+
+  The direction field (Dir) is 0 for uplink packets and 1 for downlink packets.
+  The blocks Ai are encrypted to get a sequence S of blocks Si:
+
+      Si = aes128_encrypt(K, Ai) for i = 1..k
+      S  = S1 | S2 | .. | Sk
+
+  Encryption and decryption of the payload is done by truncating
+
+      (pld | pad16) xor S
+
+  to the first len(pld) octets.
+  """
+  def decrypt(packet, key) do
+    dev_addr = packet.mac_payload.fhdr.dev_addr
+
+    pld = packet.mac_payload.frm_payload
+    pld_size = byte_size(pld)
+    pld_pad =
+    if rem(pld_size, 16) != 0 do
+      pad_bits = (16 - rem(pld_size, 16)) * 8
+      pld_pad = IO.iodata_to_binary [pld, <<0::size(pad_bits)>>]
+    else
+      pld
+    end
+
+    k = round(Float.ceil((pld_size) / 16))
+    blocks = for i <- 1..k do
+      ai = IO.iodata_to_binary [1, 0, 0, 0, 0, 0, dev_addr, <<packet.mac_payload.fhdr.f_cnt::little-unsigned-integer-size(32)>>, 0, <<i::8>>]
+      :crypto.block_encrypt(:aes_ecb, key, ai)
+    end
+    s = Enum.join(blocks) |> :erlang.bitstring_to_list
+    d = pld_pad |> :erlang.bitstring_to_list
+
+    for i <- 0..k * 16 - (16 - pld_size) - 1 do
+      Enum.at(d, i) ^^^ Enum.at(s, i)
     end
   end
-
 end
